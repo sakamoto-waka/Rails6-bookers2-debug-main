2cda05e8c00613842b8541fb7c0b31966e5a4c00
sakamoto-waka <wakanmhl@gmail.com>
2022-03-04 07:55:14 +0000

    質問用に残したバグ

--- app/mailers/models/user.rb
+++ app/mailers/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        # :authentication_keys => [:name]
 
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
@@ -10,8 +11,8 @@ class User < ApplicationRecord
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
-  
-  
+
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
