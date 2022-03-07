ba96a6ea6df6503e5b43c411fba48bb52b4d594b
sakamoto-waka <wakanmhl@gmail.com>
2022-03-07 01:50:38 +0000

    [add] searches controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,11 @@
 class SearchesController < ApplicationController
   
   def search
-    
+    @range = params[:range]
+    if @range == User
+      @user = User.looks(params[:search], params[:word])
+    else  
+      @book = Book.look(params[:search], params[:word])
+    end  
   end
 end
