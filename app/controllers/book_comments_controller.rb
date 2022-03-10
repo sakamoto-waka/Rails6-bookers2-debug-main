78757116b0ead2c793168a1a08cd19be76c01f0c
sakamoto-waka <wakanmhl@gmail.com>
2022-03-10 01:50:53 +0000

    [update] delete:destroy.js, rename: create.js → book_comments.js

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,8 +1,8 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.new(book_comment_params)
-    book_comment.book_id = book.id
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    book_comment.book_id = @book.id
     book_comment.save
     redirect_back(fallback_location: books_path)
   end
